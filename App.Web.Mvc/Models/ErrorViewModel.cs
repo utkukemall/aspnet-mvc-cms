bbfba8ec85ed4404ff054e4125bbca3c38eff676
alparslanKobak <alparslankobak@gmail.com>
2023-07-03 22:27:15 +0300

    Revert "Merge pull request #3 from utkukemall/Damlaa752"
    This reverts commit a2a3d851d27c0e4de4869db0525d1189cbb5483c, reversing
    changes made to d52a95cfcbe273e11e400f912ed6ba5ff9e20701.

--- App.Web.Mvc/Models/ErrorViewModel.cs
+++ App.Web.Mvc/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace App.Web.Mvc.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-       // public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
