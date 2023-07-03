c29a994c909b4c269d8ecec4bac4bd935f30bcd4
Damlaa752 <gultekindamla7@gmail.com>
2023-07-03 21:37:14 +0300

    Deneme12

--- App.Web.Mvc/Models/ErrorViewModel.cs
+++ App.Web.Mvc/Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace App.Web.Mvc.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+       // public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
