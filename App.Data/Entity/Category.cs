2c6d4d9a75eff29406baf89288b565d11af4bc6a
alparslanKobak <alparslankobak@gmail.com>
2023-07-06 02:58:11 +0300

    Entity

---
+++ App.Data/Entity/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Data.Entity
+{
+    public class Category
+    {
+
+        [Key] // Id property'sinin primary key olduğunu belirtir.
+
+        public int Id { get; set; }
+
+        [MaxLength(100), MinLength(2) , Required(ErrorMessage = "The {0} field cannot be left blank!")]
+
+        public string Name { get; set; }
+
+        [MaxLength(200), MinLength(1), Required(ErrorMessage = "The {0} field cannot be left blank!")]
+
+        public string Description { get; set; }
+    }
+}
