6935dde83d18db12d5f6d7a5ff5e90b13b7e8a2f
alparslanKobak <alparslankobak@gmail.com>
2023-07-06 18:31:54 +0300

    - DbContext işlemleri

---
+++ App.Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using App.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Data
+{
+	public class AppDbContext : DbContext
+	{
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<CategoryPost> CategoryPosts { get; set; }
+
+        public DbSet<Page> Pages { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<PostComment> PostComments { get; set; }
+
+        public DbSet<PostImage> PostImages { get; set; }
+
+        public DbSet<Setting> Settings { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
