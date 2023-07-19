8f90a26a7842ed6aa435b9845e2ec06ba29c06da
alparslanKobak <alparslankobak@gmail.com>
2023-07-19 21:51:11 +0300

    Test-test1

--- App.API/Program.cs
+++ App.API/Program.cs
@@ -22,7 +22,6 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
