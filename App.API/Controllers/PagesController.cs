499fb8ae260c31aac657506de3dbeb9b5a5b5282
alparslanKobak <alparslankobak@gmail.com>
2023-07-20 15:21:48 +0300

    Bazı dosyalar üzerinde mülahazatı şamil edildi

---
+++ App.API/Controllers/PagesController.cs
@@ -0,0 +1,61 @@
+﻿using App.Data.Entity;
+using App.Service.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace App.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PagesController : ControllerBase
+    {
+        private readonly IService<Page> _service;
+
+        public PagesController(IService<Page> service)
+        {
+            _service = service;
+        }
+
+        // GET: api/<PagesController>
+        [HttpGet]
+        public async Task<IEnumerable<Page>> Get()
+        {
+            return await _service.GetAllAsync();
+        }
+
+        // GET api/<PagesController>/5
+        [HttpGet("{id}")]
+        public async Task<Page> Get(int id)
+        {
+          return  await _service.FindAsync(id);
+        }
+
+        // POST api/<PagesController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Page value)
+        {
+            await _service.AddAsync(value);
+            var response = await _service.SaveAsync();
+
+            if (response>0)
+            {
+                return Ok();
+            }
+
+            return Problem();
+        }
+
+        // PUT api/<PagesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PagesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
