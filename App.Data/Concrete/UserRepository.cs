8579f80fc395b85b72e4a0970f4e815b189a0603
AltinAY16 <altinaykadir016@gmail.com>
2023-07-21 15:39:15 +0300

    Hatalı...

--- App.Data/Concrete/UserRepository.cs
+++ App.Data/Concrete/UserRepository.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using App.Data.Abstract;
+using App.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace App.Data.Concrete
 {
-    public class UserRepository
+    public class UserRepository : Repository<User>, IUserRepository
     {
+        public UserRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<User> GetUserByIncludeAsync(int userId)
+        {
+            return await _dbSet.Where(u => u.Id == userId).Include(u => u.Role).FirstOrDefaultAsync();
+        }
     }
 }
