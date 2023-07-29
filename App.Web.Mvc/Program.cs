454ec864c1aa1ed971dfaba0610e329f709a3124
utkukemall <utkukemal1999@gmail.com>
2023-07-29 18:09:45 +0300

    a

--- App.Web.Mvc/Program.cs
+++ App.Web.Mvc/Program.cs
@@ -59,66 +59,61 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-//using (var scope = app.Services.CreateScope())
-//{
-//    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>(); // Uygulama ayaða kalktýðýnda, belirtilen Database'i getir.
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>(); // Uygulama ayaða kalktýðýnda, belirtilen Database'i getir.
 
-//    var db = dbContext.Database; 
+    var db = dbContext.Database;
 
-//    if(!await db.CanConnectAsync()) // Eðer ilgili database'yi bulamýyorsan 
-//    {
-//        await db.EnsureCreatedAsync();
+    if (!await db.CanConnectAsync()) // Eðer ilgili database'yi bulamýyorsan 
+    {
+        await db.EnsureCreatedAsync();
+        // TODO: eðer veritabaný sýfýrdan oluþturulunca
+        // içerisindeki bazý tablolarda kayýt olmasý gerekiyorsa
+        // burada seed yapýlmalý
+        User admin = new()
+        {
+            Email = "admin@noeva.com",
+            City = "Çorum",
+            Id = 1,
+            Password = "123456",
+            Phone = "5469389421"
+        };
 
-//        // TODO: eðer veritabaný sýfýrdan oluþturulunca
-//        // içerisindeki bazý tablolarda kayýt olmasý gerekiyorsa
-//        // burada seed yapýlmalý
+        User doctor = new()
+        {
+            Email = "doctor@noeva.com",
+            City = "Çanakkale",
+            Id = 2,
+            Password = "123456",
+            Phone = "54693190421"
+        };
 
+        Department kardiyoloji = new()
+        {
+            Id = 1,
+            Description = "Cardio and other things",
+            Name = "Cardiology"
 
+        };
 
-//        User admin = new()
-//        {
-//            Email = "admin@noeva.com",
-//            City = "Çorum",
-//            Id = 1,
-//            Password ="123456",
-//            Phone = "5469389421"
-//        };
+        Post post = new()
+        {
+            Content = "Our Hospital is the best",
+            Id = 1,
+            Title = "Best Hospital",
+            UserId = 2
+        };
 
-//        User doctor = new()
-//        {
-//            Email = "doctor@noeva.com",
-//            City = "Çanakkale",
-//            Id = 2,
-//            Password = "123456",
-//            Phone = "54693190421"
-//        };
+        DepartmentPost departmentPost = new()
+        {
+            Id = 1,
+            DepartmentId = 1,
+            PostId = 1
+        };
+    }
+}
 
-//        Department kardiyoloji = new()
-//        {
-//            Id=1,
-//            Description = "Cardio and other things",
-//            Name = "Cardiology"
-
-//        };
-
-
-//        Post post = new()
-//        {
-//            Content = "Our Hospital is the best",
-//            Id = 1,
-//            Title = "Best Hospital",
-//            UserId = 2
-//        };
-
-//        DepartmentPost departmentPost = new()
-//        {
-//            Id = 1,
-//            DepartmentId = 1,
-//            PostId = 1
-//        };
-//    }
-//}
-
-// Category ve Services(Hospital Services) kýsmýný güncelle.
+//Category ve Services(Hospital Services) kýsmýný güncelle.
 
 app.Run();
