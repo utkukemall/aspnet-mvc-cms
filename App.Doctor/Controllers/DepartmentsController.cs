5e3ea4b10645f7e230574f3edbea0b38d2095c0b
utkukemall <utkukemal1999@gmail.com>
2023-07-29 17:42:29 +0300

    asd

---
+++ App.Doctor/Controllers/DepartmentsController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Admin.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiAdres= "http://localhost:5005/api/Departments";
+
+        public DepartmentsController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // GET: DepartmentsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DepartmentsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DepartmentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DepartmentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DepartmentsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DepartmentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DepartmentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DepartmentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
