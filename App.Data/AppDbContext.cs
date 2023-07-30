50ee539fb199734e78a72211265a49d8b665b514
AltinAY16 <altinaykadir016@gmail.com>
2023-07-30 19:10:55 +0300

    Doctor and Patient

--- App.Data/AppDbContext.cs
+++ App.Data/AppDbContext.cs
@@ -7,17 +7,13 @@ namespace App.Data
 	public class AppDbContext : DbContext
 	{
         public DbSet<Department> Departments { get; set; }
-
         public DbSet<DepartmentPost> DepartmentPosts { get; set; }
-
         public DbSet<Page> Pages { get; set; }
-
+        public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Post> Posts { get; set; }
-
+        public DbSet<Patient> Patients { get; set; }
         public DbSet<PostComment> PostComments { get; set; }
-
         public DbSet<PostImage> PostImages { get; set; }
-
         public DbSet<Setting> Settings { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
