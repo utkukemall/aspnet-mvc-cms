95c41689dc71880ea6bce85b5f2c39a312b4b366
alparslanKobak <alparslankobak@gmail.com>
2023-07-31 21:05:13 +0300

    .

---
+++ App.Data/Abstract/IDoctorRepository.cs
@@ -0,0 +1,19 @@
+﻿using App.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Abstract
+{
+    public interface IDoctorRepository : IRepository<Doctors>
+    {
+        Task<Doctors> GetDoctorByIncludeAsync(int id);
+
+        Task<List<Doctor>> GetAllDoctorsByIncludeAsync();
+
+        Task<List<Doctor>> GetSomeDoctorsByIncludeAsync(Expression<Func<Doctors,bool>> expression);
+    }
+}
