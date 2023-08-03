e6478181ba851810280575e5fe6f3d43de999ecb
alparslanKobak <alparslankobak@gmail.com>
2023-08-03 17:14:37 +0300

    Image

---
+++ App.Data/Abstract/IDepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using App.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Abstract
+{
+    public interface IDepartmentRepository : IRepository<Department>
+    {
+        Task<Department> GetDepartmentByIncludeAsync(int id);
+
+        Task<List<Department>> GetAllDepartmentsByIncludeAsync();
+
+        Task<List<Department>> GetSomeDepartmentsByIncludeAsync(Expression<Func<Department, bool>> expression);
+    }
+}
