dadfab1a9ec269f4d3cbfdc84172b6f160b69fca
alparslanKobak <alparslankobak@gmail.com>
2023-08-03 15:18:40 +0300

    .

--- App.API/Controllers/DepartmentsController.cs
+++ App.API/Controllers/DepartmentsController.cs
@@ -5,71 +5,77 @@ using Newtonsoft.Json.Linq;
 
 namespace App.API.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class DepartmentsController : ControllerBase
-	{
-		private readonly IService<Department> _service;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IService<Department> _service;
 
-		public DepartmentsController(IService<Department> departmentContext)
-		{
-			_service = departmentContext;
-		}
+        public DepartmentsController(IService<Department> departmentContext)
+        {
+            _service = departmentContext;
+        }
 
-		// GET: api/<DepartmentsController>
-		[HttpGet]
-		public async Task<IEnumerable<Department>> Get()
-		{
-			return await _service.GetAllAsync();
-		}
+        // GET: api/<DepartmentsController>
+        [HttpGet]
+        public async Task<IEnumerable<Department>> Get()
+        {
+            return await _service.GetAllAsync();
+        }
 
-		// GET api/<DepartmentsController>/5
-		[HttpGet("{id}")]
-		public async Task<Department> Get(int id)
-		{
-			return await _service.FindAsync(id);
-		}
+        // GET api/<DepartmentsController>/5
+        [HttpGet("{id}")]
+        public async Task<Department> Get(int id)
+        {
+            return await _service.FindAsync(id);
+        }
 
-		// POST api/<DepartmentsController>
-		[HttpPost]
-		public async Task<ActionResult> Post([FromBody] Department value)
-		{
-			await _service.AddAsync(value);
+        // POST api/<DepartmentsController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Department value)
+        {
+            await _service.AddAsync(value);
             await _service.SaveAsync();
             return Ok(value);
-		}
+        }
 
-		// PUT api/<DepartmentsController>/5
-		[HttpPut("{id}")]
-		public async Task<ActionResult> Put(int id, [FromBody] Department value)
-		{
-			Department mainDepartment = await _service.FindAsync(id);
+        // PUT api/<DepartmentsController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Department value)
+        {
+            Department mainDepartment = await _service.FindAsync(id);
 
-			if(mainDepartment != null)
-			{
-				mainDepartment.Name = value.Name;
-				mainDepartment.Description = value.Description;
-				mainDepartment.Image = value.Image;
+            if (mainDepartment != null)
+            {
+                mainDepartment.Name = value.Name;
+                mainDepartment.Description = value.Description;
+                mainDepartment.Image = value.Image;
 
-				_service.Update(mainDepartment);
-				 await _service.SaveAsync();
-				return Ok(mainDepartment);
+                _service.Update(mainDepartment);
+                await _service.SaveAsync();
+                return Ok(mainDepartment);
             }
-			return NotFound();
-		}
+            return NotFound();
+        }
 
-		// DELETE api/<DepartmentsController>/5
-		[HttpDelete("{id}")]
-		public async Task<ActionResult> Delete(int id)
-		{
-			var mainDepartment = await _service.FindAsync(id);
-			if (mainDepartment != null)
-			{
-				_service.Delete(mainDepartment);
-				await _service.SaveAsync();
-				return Ok();
-			}
-			return NotFound(mainDepartment);
-		}
-	}
+        // DELETE api/<DepartmentsController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var mainDepartment = await _service.FindAsync(id);
+            if (mainDepartment != null)
+            {
+                _service.Delete(mainDepartment);
+                var response = await _service.SaveAsync();
+                if (response > 0)
+                {
+                    return Ok();
+
+                }
+                return Problem();
+
+            }
+            return NotFound();
+        }
+    }
 }
