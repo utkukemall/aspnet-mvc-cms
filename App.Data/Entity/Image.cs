cc1cb5ed4cc6cf2a02ec19418cf390ea4f3c319e
alparslanKobak <alparslankobak@gmail.com>
2023-08-03 16:00:01 +0300

    .

--- App.Data/Entity/Image.cs
+++ App.Data/Entity/Image.cs
@@ -6,6 +6,13 @@ namespace App.Data.Entity
 	public class Image : BaseEntity
 	{
         [MaxLength(200, ErrorMessage = "The {0} cannot exceed 200 characters."), MinLength(1, ErrorMessage = "The {0} must be at least 1 characters."), Required(ErrorMessage = "The {0} field cannot be left blank!"), DataType(DataType.ImageUrl), Column(TypeName ="nvarchar(200)")]
-        public string? ImagePath { get; set; }     
-	}
+        public string? ImagePath { get; set; }
+
+        public Title? ImageTitle { get; set; }
+
+        public enum Title
+        {
+            Empty, Profile, Post, Department
+        }
+    }
 }
--- App.Web.Mvc/ViewComponents/Departments.cs
+++ App.Web.Mvc/ViewComponents/Departments.cs
@@ -7,18 +7,17 @@ namespace App.Web.Mvc.ViewComponents
 	{
 		private readonly HttpClient _httpClient;
 
-		public Departments(HttpClient httpClient)
+        private readonly string _apiAddress = "http://localhost:5005/api/Departments";
+        public Departments(HttpClient httpClient)
 		{
 			_httpClient = httpClient;
 		}
 
 
-		// Apı Adresi gelecek
-
-		//public async Task<IViewComponentResult> InvokeAsync()
-		//{
-		//	List<Department> departments = await _httpClient.GetFromJsonAsync<List<Department>>("      {Apiadresi gelecek} ");
-		//	return View(departments);
-		//}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var model = await _httpClient.GetFromJsonAsync<List<Department>>(_apiAddress);
+			return View(model);
+		}
 	}
 }
