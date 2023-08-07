5202b5205f9124e29e087c3598fdae695ce31bc4
alparslanKobak <alparslankobak@gmail.com>
2023-08-07 14:20:08 +0300

    .

--- App.Admin/Controllers/AuthController.cs
+++ App.Admin/Controllers/AuthController.cs
@@ -17,72 +17,75 @@ namespace App.Admin.Controllers
 
         private readonly string _apiAddress = "http://localhost:5005/api/Users";
         private readonly string _apiRoleAddress = "http://localhost:5005/api/Roles";
-        public IActionResult Register()
-        {
-            return View();
-        }
-        [HttpPost]
 
 
-        public async Task<IActionResult> Register(User newUser, string password2)
-        {
-            try
-            {
-                if (newUser.Password == password2)
-                {
-                    var users = await _httpClient.GetFromJsonAsync<List<User>>(_apiAddress);
-                    var user = users.FirstOrDefault(u => u.Email == newUser.Email);
+        //public IActionResult Register()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
 
-                    if (user is not null)
-                        ModelState.AddModelError("", "This Email Has Already Been Registered!");
 
-                    var roles = await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoleAddress);
+        //public async Task<IActionResult> Register(User newUser, string password2)
+        //{
+        //    try
+        //    {
+        //        if (newUser.Password == password2)
+        //        {
+        //            var users = await _httpClient.GetFromJsonAsync<List<User>>(_apiAddress);
+        //            var user = users.FirstOrDefault(u => u.Email == newUser.Email);
 
-                    // Burada, doktor, kullanıcı ve admin rollerinden birini seçelim.
-                    Role selectedRole = SelectRoleToAssign(roles);
+        //            if (user is not null)
+        //                ModelState.AddModelError("", "This Email Has Already Been Registered!");
 
-                    if (selectedRole == null)
-                    {
-                        ModelState.AddModelError("", "No valid role found to assign to the new user.");
-                        return View(newUser);
-                    }
+        //            var roles = await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoleAddress);
 
-                    newUser.RoleId = selectedRole.Id; // Eğer Id kullanıyorsanız RoleId'ye atayın, ya da RoleName'e göre yapıyorsanız RoleName'e atayın.
+        //            // Burada, doktor, kullanıcı ve admin rollerinden birini seçelim.
+        //            Role selectedRole = SelectRoleToAssign(roles);
 
-                    var add = await _httpClient.PostAsJsonAsync(_apiAddress, newUser);
+        //            if (selectedRole == null)
+        //            {
+        //                ModelState.AddModelError("", "No valid role found to assign to the new user.");
+        //                return View(newUser);
+        //            }
 
-                    if (add.IsSuccessStatusCode)
-                        return RedirectToAction("Main", "Home");
-                    else
-                        ModelState.AddModelError("", "An error occurred while registering the user.");
-                }
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError("", ex.Message);
-            }
-            return View(newUser);
-        }
+        //            newUser.RoleId = selectedRole.Id; // Eğer Id kullanıyorsanız RoleId'ye atayın, ya da RoleName'e göre yapıyorsanız RoleName'e atayın.
+
+        //            var add = await _httpClient.PostAsJsonAsync(_apiAddress, newUser);
+
+        //            if (add.IsSuccessStatusCode)
+        //                return RedirectToAction("Main", "Home");
+        //            else
+        //                ModelState.AddModelError("", "An error occurred while registering the user.");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        ModelState.AddModelError("", ex.Message);
+        //    }
+        //    return View(newUser);
+        //}
 
         // Belirtilen üç rol (doktor, kullanıcı ve admin) arasından birini seçen yardımcı fonksiyon.
-        private Role SelectRoleToAssign(List<Role> roles)
+        //private Role SelectRoleToAssign(List<Role> roles)
+        //{
+
+        //    // Varsayılan olarak, doktor rolünü döndürelim:
+        //    return roles.FirstOrDefault(r => r.RoleName == "doktor");
+        //}
+
+
+        public async Task<IActionResult> Logout()
         {
-
-            // Varsayılan olarak, doktor rolünü döndürelim:
-            return roles.FirstOrDefault(r => r.RoleName == "doktor");
+            await HttpContext.SignOutAsync();
+            return RedirectToAction(nameof(Login),"Auth");
         }
-
-
-
 
 
 
 
         public IActionResult Login()
         {
-
-
-
             return View();
         }
 
