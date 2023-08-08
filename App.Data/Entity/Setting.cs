1cb2ff65ae7b04dd769509db1d1aa726909ed354
alparslanKobak <alparslankobak@gmail.com>
2023-08-08 17:43:26 +0300

    .

--- App.Data/Entity/Setting.cs
+++ App.Data/Entity/Setting.cs
@@ -7,7 +7,7 @@ namespace App.Data.Entity
     {
 
         [MaxLength(200, ErrorMessage = "The {0} cannot exceed 200 characters."), MinLength(2, ErrorMessage = "The {0} must be at least 2 characters."), Required(ErrorMessage = "The {0} field cannot be left blank!"), Column(TypeName = "nvarchar(200)")] // Error Message kısımları işlevsel olarak düzenlenerek eklenebilir.
-        public string Name { get; set; }
+        public string Name { get; set; } 
 
         [MaxLength(400, ErrorMessage = "The {0} cannot exceed 400 characters."), MinLength(2, ErrorMessage = "The {0} must be at least 2 characters."), Required(ErrorMessage = "The {0} field cannot be left blank!"), Column(TypeName = "nvarchar(400)")]
         public string Value { get; set; }
---
+++ App.Web.Mvc/Models/HomePageViewModels.cs
@@ -0,0 +1,13 @@
+﻿using App.Data.Entity;
+
+namespace App.Web.Mvc.Models
+{
+    public class HomePageViewModels
+    {
+        public Setting? setting { get; set; }
+
+        public Post? post { get; set; }
+
+
+    }
+}
