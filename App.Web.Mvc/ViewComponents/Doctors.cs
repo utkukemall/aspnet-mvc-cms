71dd9099fbcec0746230f553c9d71e6b76b95b9a
alparslanKobak <alparslankobak@gmail.com>
2023-08-09 16:17:05 +0300

    çekişmeli

--- App.Web.Mvc/ViewComponents/Doctors.cs
+++ App.Web.Mvc/ViewComponents/Doctors.cs
@@ -3,22 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace App.Web.Mvc.ViewComponents
 {
-    public class Departments : ViewComponent
+    public class Doctors : ViewComponent
     {
         private readonly HttpClient _httpClient;
 
         private readonly string _apiAddress = "http://localhost:5005/api/Departments";
-        public Departments(HttpClient httpClient)
+        public Doctors(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
 
-
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var model = await _httpClient.GetFromJsonAsync<List<Department>>(_apiAddress);
-            model.OrderBy(d=> d.Name);
+            model.OrderBy(d => d.Name);
             return View(model);
         }
+
     }
 }
