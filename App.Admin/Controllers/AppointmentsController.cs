1fc15d254fed15ffd17b7258f1a1a263f2dd1944
utkukemall <utkukemal1999@gmail.com>
2023-08-09 14:58:11 +0300

    asd

--- App.Admin/Controllers/AppointmentsController.cs
+++ App.Admin/Controllers/AppointmentsController.cs
@@ -1,6 +1,7 @@
 ﻿using App.Data.Entity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace App.Admin.Controllers
 {
@@ -9,7 +10,11 @@ namespace App.Admin.Controllers
     {
 
         private readonly HttpClient _httpClient;
+
         private readonly string _apiAddress = "http://localhost:5005/api/Appointments";
+        private readonly string _apiRoles = "http://localhost:5005/api/Roles";
+        private readonly string _apiDoctorsRoles = "http://localhost:5005/api/Doctors";
+        private readonly string _apiDepartments = "http://localhost:5005/api/Departments";
 
         public AppointmentsController(HttpClient httpClient)
         {
@@ -19,71 +24,89 @@ namespace App.Admin.Controllers
         // GET: AppointmentsController
         public async Task<ActionResult> Index()
         {
+            ViewBag.RoleId = new SelectList(await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoles), "Id", "RoleName");
+            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctorsRoles), "Id", "FullName");
+            ViewBag.DepartmentId = new SelectList(await _httpClient.GetFromJsonAsync<List<Department>>(_apiDepartments), "Id", "Name");
             var model = await _httpClient.GetFromJsonAsync<List<Appointment>>(_apiAddress);
             return View(model);
         }
 
-        // GET: AppointmentsController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
         // GET: AppointmentsController/Create
-        public ActionResult Create()
+        public async Task<ActionResult> Create()
         {
+            ViewBag.RoleId = new SelectList(await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoles), "Id", "RoleName");
+            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctorsRoles), "Id", "FullName");
+            ViewBag.DepartmentId = new SelectList(await _httpClient.GetFromJsonAsync<List<Department>>(_apiDepartments), "Id", "Name");
             return View();
         }
 
         // POST: AppointmentsController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create(Appointment collection)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var response = await _httpClient.PostAsJsonAsync(_apiAddress, collection);
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["Message"] = "<div class='alert alert-success'>The Job is Done Sir!</div>";
+                    return RedirectToAction(nameof(Index));
+
+                }
             }
-            catch
+            catch (Exception e)
             {
-                return View();
+                ModelState.AddModelError("", "Hata oluştu : " + e.Message);
             }
+            ViewBag.RoleId = new SelectList(await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoles), "Id", "RoleName");
+            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctorsRoles), "Id", "FullName");
+            ViewBag.DepartmentId = new SelectList(await _httpClient.GetFromJsonAsync<List<Department>>(_apiDepartments), "Id", "Name");
+            return View(collection);
         }
 
         // GET: AppointmentsController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> Edit(int? id)
         {
-            return View();
+            ViewBag.RoleId = new SelectList(await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoles), "Id", "RoleName");
+            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctorsRoles), "Id", "FullName");
+            ViewBag.DepartmentId = new SelectList(await _httpClient.GetFromJsonAsync<List<Department>>(_apiDepartments), "Id", "Name");
+            var model = await _httpClient.GetFromJsonAsync<Patient>(_apiAddress + "/" + id);
+            return View(model);
         }
 
         // POST: AppointmentsController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> Edit(int id, Patient collection)
         {
-            try
+            var response = await _httpClient.PutAsJsonAsync(_apiAddress + "/" + id, collection);
+
+            if (response.IsSuccessStatusCode)
             {
-                return RedirectToAction(nameof(Index));
+                TempData["Message"] = "<div class='alert alert-success'>The Job is Done Sir!</div>";
             }
-            catch
-            {
-                return View();
-            }
+            ViewBag.RoleId = new SelectList(await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoles), "Id", "RoleName");
+            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctorsRoles), "Id", "FullName");
+            ViewBag.DepartmentId = new SelectList(await _httpClient.GetFromJsonAsync<List<Department>>(_apiDepartments), "Id", "Name");
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: AppointmentsController/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Remove(int id)
         {
-            return View();
+            var model = await _httpClient.GetFromJsonAsync<Patient>(_apiAddress + "/" + id);
+            return View(model);
         }
 
         // POST: AppointmentsController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Remove(int id, Patient collection)
         {
             try
             {
+                await _httpClient.DeleteAsync(_apiAddress + "/" + id);
                 return RedirectToAction(nameof(Index));
             }
             catch
