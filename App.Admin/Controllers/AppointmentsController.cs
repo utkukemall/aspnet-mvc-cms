0a72b989027e78da50d6f9381673ccc489fd7544
alparslanKobak <alparslankobak@gmail.com>
2023-08-11 02:41:10 +0300

    Revert "Merge branch 'KilicdaroglundakiLiderlikVasfi' into KaaDeneme"
    This reverts commit f93d2f4d0aaf83cd389fa9aebe9270111a2a2aca, reversing
    changes made to bdb07c4fbcb299c952da5ea2768bc7e4538306e7.

--- App.Admin/Controllers/AppointmentsController.cs
+++ App.Admin/Controllers/AppointmentsController.cs
@@ -13,7 +13,7 @@ namespace App.Admin.Controllers
 
         private readonly string _apiAddress = "http://localhost:5005/api/Appointments";
         private readonly string _apiRoles = "http://localhost:5005/api/Roles";
-        private readonly string _apiDoctors = "http://localhost:5005/api/Doctors";
+        private readonly string _apiDoctorsRoles = "http://localhost:5005/api/Doctors";
         private readonly string _apiDepartments = "http://localhost:5005/api/Departments";
 
         public AppointmentsController(HttpClient httpClient)
@@ -25,7 +25,7 @@ namespace App.Admin.Controllers
         public async Task<ActionResult> Index()
         {
             ViewBag.RoleId = new SelectList(await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoles), "Id", "RoleName");
-            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctors), "Id", "FullName");
+            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctorsRoles), "Id", "FullName");
             ViewBag.DepartmentId = new SelectList(await _httpClient.GetFromJsonAsync<List<Department>>(_apiDepartments), "Id", "Name");
             var model = await _httpClient.GetFromJsonAsync<List<Appointment>>(_apiAddress);
             return View(model);
@@ -35,7 +35,7 @@ namespace App.Admin.Controllers
         public async Task<ActionResult> Create()
         {
             ViewBag.RoleId = new SelectList(await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoles), "Id", "RoleName");
-            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctors), "Id", "FullName");
+            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctorsRoles), "Id", "FullName");
             ViewBag.DepartmentId = new SelectList(await _httpClient.GetFromJsonAsync<List<Department>>(_apiDepartments), "Id", "Name");
             return View();
         }
@@ -60,7 +60,7 @@ namespace App.Admin.Controllers
                 ModelState.AddModelError("", "Hata oluştu : " + e.Message);
             }
             ViewBag.RoleId = new SelectList(await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoles), "Id", "RoleName");
-            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctors), "Id", "FullName");
+            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctorsRoles), "Id", "FullName");
             ViewBag.DepartmentId = new SelectList(await _httpClient.GetFromJsonAsync<List<Department>>(_apiDepartments), "Id", "Name");
             return View(collection);
         }
@@ -69,7 +69,7 @@ namespace App.Admin.Controllers
         public async Task<ActionResult> Edit(int? id)
         {
             ViewBag.RoleId = new SelectList(await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoles), "Id", "RoleName");
-            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctors), "Id", "FullName");
+            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctorsRoles), "Id", "FullName");
             ViewBag.DepartmentId = new SelectList(await _httpClient.GetFromJsonAsync<List<Department>>(_apiDepartments), "Id", "Name");
             var model = await _httpClient.GetFromJsonAsync<Appointment>(_apiAddress + "/" + id);
             return View(model);
@@ -87,7 +87,7 @@ namespace App.Admin.Controllers
                 TempData["Message"] = "<div class='alert alert-success'>The Job is Done Sir!</div>";
             }
             ViewBag.RoleId = new SelectList(await _httpClient.GetFromJsonAsync<List<Role>>(_apiRoles), "Id", "RoleName");
-            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctors), "Id", "FullName");
+            ViewBag.DoctorId = new SelectList(await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiDoctorsRoles), "Id", "FullName");
             ViewBag.DepartmentId = new SelectList(await _httpClient.GetFromJsonAsync<List<Department>>(_apiDepartments), "Id", "Name");
             return RedirectToAction(nameof(Index));
         }
--- App.Web.Mvc/Controllers/AppointmentsController.cs
+++ App.Web.Mvc/Controllers/AppointmentsController.cs
@@ -1,9 +1,6 @@
 ﻿using App.Data.Entity;
-using App.Web.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Doctors = App.Data.Entity.Doctors;
 
 namespace App.Web.Mvc.Controllers
 {
@@ -11,8 +8,6 @@ namespace App.Web.Mvc.Controllers
     {
         private readonly HttpClient _httpClient;
         private readonly string _apiAddress = "http://localhost:5005/api/Appointments";
-        private readonly string _apiAddressDepartments = "http://localhost:5005/api/Departments";
-        private readonly string _apiAddressDoctors = "http://localhost:5005/api/Doctors";
 
         public AppointmentsController(HttpClient httpClient)
         {
@@ -21,12 +16,10 @@ namespace App.Web.Mvc.Controllers
 
 
         // GET: AppointmentsController/Create
-        public async Task<IActionResult> Create()
+        public ActionResult Create()
         {
-            ViewBag.DepartmentId = new SelectList(await _httpClient.GetFromJsonAsync<List<Department>>(_apiAddressDepartments), "Id", "Name");
             return View();
         }
-
 
         // POST: AppointmentsController/Create
         [HttpPost]
@@ -36,33 +29,15 @@ namespace App.Web.Mvc.Controllers
             try
             {
                 var response = await _httpClient.PostAsJsonAsync(_apiAddress, collection);
-                if (response.IsSuccessStatusCode)
-                {
-                    TempData["Message"] = "<div class='alert alert-success'>Your Appointment has been created... Thank you for choosing us</div>";
-                    return RedirectToAction("Index", "Home");
-
-                }
-                TempData["Message"] = "<div class='alert alert-success'>Error, Please Try Again! </div>";
-                return View(collection);
+                TempData["Message"] = "<div class='alert alert-success'>Your Appointment has been created... Thank you for choosing us</div>";
+                return RedirectToAction("Index","Home");
             }
             catch
             {
-                ModelState.AddModelError("", "Your Appointment cannot sended. Please Try Again!");
-                return View(collection);
+                return View();
             }
         }
 
-        public async Task<JsonResult> LoadDoctor(int departmentId)
-        {
-            var doctorlist = await _httpClient.GetFromJsonAsync<List<Doctors>>(_apiAddressDoctors);
-
-            var newDoctors = doctorlist.Where(d => d.DepartmentId == departmentId).ToList();
-
-
-            return Json(new SelectList(newDoctors, "Id", "FullName"));
-        }
-
-
-
+       
     }
 }
