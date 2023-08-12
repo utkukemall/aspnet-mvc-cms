0131e0d71d69a4ecdb743a07d248e9441b44e895
alparslanKobak <alparslankobak@gmail.com>
2023-08-12 13:49:55 +0300

    .

--- App.API/Abstarct/IFileService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace App.API.Abstract
-{
-    public interface IFileService
-    {
-        public Tuple<int, string> SaveImage(IFormFile image);
-        public bool DeleteImage(string imageFileName);
-    }
-}
