1c5673d019a4074046086203c2b3c7a9acdf1dbf
alparslanKobak <alparslankobak@gmail.com>
2023-08-17 02:07:12 +0300

    Revert "."
    This reverts commit 6667edd9f66470e043792669cde2b81e2cdb0e24.

--- App.Web.Mvc/Controllers/AuthController.cs
+++ App.Web.Mvc/Controllers/AuthController.cs
@@ -76,7 +76,7 @@ namespace App.Web.Mvc.Controllers
                         string imageTitle = webMvcImagePath.Substring(startIndex);
                         string imagePath = await FileHelper.FileLoaderAPI(Image, targetFolderPath, imageTitle);
                         string doctorImagePath = await FileHelper.FileLoaderDoctor(Image, doctorFolderPath, imageTitle);
-                        newUser.Image = imagePath; 
+                        newUser.Image = imagePath; //çek
 
                         if (!Directory.Exists(adminTargetFilePath))
                         {
@@ -85,7 +85,7 @@ namespace App.Web.Mvc.Controllers
                     }
 
                     TempData["Message"] = "<div class='alert alert-success'>The Job is Done Sir!</div>";
-                    return RedirectToAction("Index","Home");
+                    return RedirectToAction(nameof(Index));
                 }
                 else
                 {
